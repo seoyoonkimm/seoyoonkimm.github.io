437c4007b58b9839d7d8d71ed7288818a1bbf4df
Seoyoon Kim <seoyoonkim@Seoyoons-MacBook-Pro.local>
2020-03-11 18:44:48 -0400

    Updates

---
+++ precache-manifest.113b7d28eee79c17a0cb42bdf0581e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d4a0314999243c01262a72aa7d3342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1517607234caed55642",
+    "url": "/static/css/main.a058d5f4.chunk.css"
+  },
+  {
+    "revision": "2a93699ad4e187c4b754",
+    "url": "/static/js/2.74e3d69d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.74e3d69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1517607234caed55642",
+    "url": "/static/js/main.b82c1e79.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
